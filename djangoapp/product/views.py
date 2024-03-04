0b2504843bf0271a82ccbecfc85ddef867b2d0cf
VCruzSant <sviniantiago@gmail.com>
2024-03-04 15:05:30 -0300

    html files created

--- djangoapp/product/views.py
+++ djangoapp/product/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
+from product.models import Product
 
 # Create your views here.
 
 
 class ProductList(ListView):
-    ...
+    model = Product
+    template_name = 'product/pages/index.html'
+    context_object_name = 'products'
+    ordering = '-pk',
+    paginate_by = 2
 
 
 class ProducDetails(DetailView):
