1ca5c207aaa07362f0f68e356b63afa632f879d4
VCruzSant <sviniantiago@gmail.com>
2024-03-17 20:23:27 -0300

    setting titles

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -47,8 +47,6 @@ INSTALLED_APPS = [
     'user_profile',
     'crispy_forms',
     'crispy_bootstrap4',
-    # debug-toolbar
-    "debug_toolbar",
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -62,8 +60,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # debug-toolbar
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -173,12 +169,6 @@ SESSION_SAVE_EVERY_REQUEST = False
 # SESSION_ENGINE = "django.contrib.sessions.backends.file"
 # SESSION_FILE_PATH = '/home/luizotavio/Desktop/temp'
 
-# debug-toolbar
-INTERNAL_IPS = [
-    # ...
-    "127.0.0.1:8000",
-    # ...
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -18,7 +18,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-import debug_toolbar
 
 urlpatterns = [
     path('', include('product.urls')),
@@ -34,4 +33,3 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
-    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
